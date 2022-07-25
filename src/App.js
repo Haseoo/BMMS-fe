22c98168c3b35349473002c0e69e12b854851310
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2022-07-25 21:42:17 +0200

    Wrap nav links with LinkContainer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState } from 'react';
+import { LinkContainer } from "react-router-bootstrap";
 
 
 
@@ -16,7 +17,6 @@ const Header = props => {
   const login = () => {
     setUserName('DS')
   }
-  console.log(location.pathname)
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -25,13 +25,21 @@ const Header = props => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto" activeKey={location.pathname}>
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/materials">Materials</Nav.Link>
-              <Nav.Link as={Link} to="/companies">Companies</Nav.Link>
-              <Nav.Link as={Link} to="/offers">Offers</Nav.Link>
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/materials">
+                <Nav.Link>Materials</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/companies">
+                <Nav.Link>Companies</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/offers">
+                <Nav.Link>Offers</Nav.Link>
+              </LinkContainer>
             </Nav>
             <Nav>
-              <NavDropdown title= {userName} id="basic-nav-dropdown">
+              <NavDropdown title={userName} id="basic-nav-dropdown">
                 <NavDropdown.Item onClick={() => login(this)}>Log in</NavDropdown.Item>
                 {/* <NavDropdown.Item>
                   Separated link
@@ -52,7 +60,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/materials" element={<Materials />} />
-        <Route path="*" element= { <h1>NOT FOUND!</h1> }/>
+        <Route path="*" element={<h1>NOT FOUND!</h1>} />
       </Routes>
     </BrowserRouter>
   );
